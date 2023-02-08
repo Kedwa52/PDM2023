c3610c869a6c4e283163057315acbf8d2c8f6f32
Kedwa52 <kedwa52@lsu.edu>
2023-02-07 21:17:21 -0600

    change folders????

--- HW3/sketch.js
+++ HW3/sketch.js
@@ -7,8 +7,8 @@ let walkingAnimationRepeat;
 
 // loads pictures before coding starts
 function preload() {
-  spriteSheet = loadImage('../Assets/SpelunkyPurple.png');
-  spriteSheet2 = loadImage('../Assets/SpelunkyMonk.png');
+  spriteSheet = loadImage("assets/SpelunkyPurple.png");
+  spriteSheet2 = loadImage("assets/SpelunkyMonk.png");
 }
 
 function setup() {
